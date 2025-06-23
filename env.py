972fdf1248d327eb81f1ce2d02dcf2a1ef492ef8
MV <mvasilev801@gmail.com>
2025-06-24 00:41:49 +0300

    Add CartPole environment implementation with reset and step methods

---
+++ env.py
@@ -0,0 +1,51 @@
+import gymnasium as gym
+import numpy as np
+import multiprocessing as mp
+
+class CartPoleEnv(gym.Env):
+    
+    def __init__(self, render=False):
+        super(CartPoleEnv, self).__init__()
+        self.render_mode = "human" if render else None
+        self.state = None
+        low = np.array([-5, -np.inf, -30*np.pi/180, -np.inf])
+        high = -low
+        self.observation_space = gym.spaces.Box(low, high, dtype=np.float32)
+        self.action_space = gym.spaces.Discrete(2)
+
+    def reset(self):
+        super().reset()
+        self.state = np.random.uniform(low=-0.05, high=0.05, size=(4,)).astype(np.float32)
+        return self.state, {}
+
+    def step(self, action):
+        dt = 0.02
+        g = 9.8
+        m = 0.5
+        M = 1.0
+        l = 0.5
+        x, x_dot, theta, theta_dot = self.state
+        F = 10 if  action == 1 else -10
+        sin_theta = np.sin(theta)
+        cos_theta = np.cos(theta)
+        temp = (F + m*l*sin_theta*theta_dot**0.5) / (M + m)
+        theta_ddot = (g * sin_theta - cos_theta * temp) / (l * (4.0 / 3.0 - m * np.square(cos_theta) / (M + m)))
+        x_ddot = temp - l * theta_ddot * cos_theta / (M + m)
+        x = x + x_dot * dt
+        x_dot = x_dot + x_ddot * dt
+        theta = theta + theta_dot * dt
+        theta_dot = theta_dot + theta_ddot * dt
+        terminated = bool(x < -2.4 or x > 2.4 or theta < -12 * np.pi / 180 or theta > 12 * np.pi / 180)
+        reward = 1.0 if not terminated else 0.0
+        
+        if self.render_mode == "human":
+            self.render()
+        self.state = np.array([x, x_dot, theta, theta_dot], dtype=np.float32)
+        
+        return self.state, reward, terminated, False, {}
+
+    def close(self):
+        return super().close()
+
+    def render(self):
+        pass
